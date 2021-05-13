ee3370cf324575ee3567a48db660786a130f4322
Emmanoel Coutinho <emmanoelcesar967@gmail.com>
2021-05-13 01:15:35 -0300

    frist commit, configs

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+
+import GlobalStyles from "../styles/globals";
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Component {...pageProps} />
+    <GlobalStyles />
+  </>
+);
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>teste</title>
+      </Head>
+      <h1>Testando eslint</h1>
+    </>
+  );
+};
+
+export default Home;
